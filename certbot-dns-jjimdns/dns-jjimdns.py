aa35e9944bb529248bab553f3ba11615ddde80d5
root on utena.wsl.jjim.de <julia@jjim.de>
2020-03-24 15:26:35 +0100

    init

---
+++ certbot-dns-jjimdns/dns-jjimdns.py
@@ -0,0 +1,31 @@
+
+import subprocess
+
+import zope.interface
+
+from certbot import errors
+from certbot import interfaces
+from certbot.plugins import dns_common
+
+logger = logging.getLogger(__name__)
+
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+
+class JJIMDNS_Authenticator(dns_common.DNSAuthenticator):
+    default_ttl=30
+
+    @classmethod
+    def add_parser_arguments(cls, add):  # pylint: disable=arguments-differ
+        super(Authenticator, cls).add_parser_arguments(add, default_propagation_seconds=0)
+        add('remote_host', help='host to connect to via SSH', default='localhost')
+        add('remote_user', help='user to connect as', default='root')
+        add('min_ttl', help='minimum record TTL', default=30, type=int)
+
+    def _setup_credentials(self):
+        return
+
+    def _perform(self, domain, rname, content):
+        subprocess.run([ "ssh", "{}@{}".format(self.conf("remote_user"), self.conf("remote_host")), "replace: {} {} IN TXT {}".format(rname, self.conf("min_ttl"), content )])
+    def _cleanup(self, domain, rname, content):
+        subprocess.run([ "ssh", "{}@{}".format(self.conf("remote_user"), self.conf("remote_host")), "delete: {} {} IN TXT {}".format(rname, self.conf("min_ttl"), content )])
---
+++ setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('README.md') as file:
+    long_description = file.read()
+
+version = '0.0.2'
+
+# Remember to update local-oldest-requirements.txt when changing the minimum
+# acme/certbot version.
+install_requires = [
+    'acme>=0.21.1',
+    'certbot>=0.21.1',
+    'setuptools',
+    'zope.interface',
+]
+
+docs_extras = [
+    'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
+    'sphinx_rtd_theme',
+]
+
+setup(
+    name='certbot-dns-jjimdns',
+    version=version,
+    description="DNS Authenticator plugin proxying records via SSH",
+    url='',
+    author="Julia Brunenberg",
+    license='PD',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/x-md',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'docs': docs_extras,
+    },
+    entry_points={
+        'certbot.plugins': [
+            'dns-jjimdns = certbot_dns_jjimdns.dns_jjimdns:JJIMDNS_Authenticator',
+        ],
+    },
+)
